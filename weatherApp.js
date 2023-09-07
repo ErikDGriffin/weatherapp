a0211682f0e0af752747952da43d6344815e1c39
erik Griffin <griffin.erikd@gmail.com>
2023-09-07 16:40:11 -0400

    updated app

--- weatherApp.js
+++ weatherApp.js
@@ -21,58 +21,74 @@ class SelectedAreas {
     return this.selectedAreas;
   }
 }
+
 function WeatherApp() {
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
   const [selectedAreas, setSelectedAreas] = useState(new SelectedAreas());
+  const [error, setError] = useState(null);
 
   const handleSearch = async () => {
     try {
+      setError(null);
+
       const apiKey = '4f687960aac453f859c90cdb61eb395b';
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
       );
-      
+
+      if (!response.ok) {
+        throw new Error(`Weather data request failed with status ${response.status}`);
+      }
+
       const data = await response.json();
       setWeatherData(data);
 
       setSelectedAreas((prevSelectedAreas) => {
         const newSelectedAreas = new SelectedAreas();
         newSelectedAreas.selectedAreas = [...prevSelectedAreas.getAreas()];
-        newSelectedAreas.addArea(city);
+        newSelectedAreas.addArea(`${city} (${new Date().toLocaleTimeString()})`);
         return newSelectedAreas;
       });
     } catch (error) {
+      setError(error.message);
       console.error('Error fetching weather data:', error);
     }
   };
 
   const handleRefresh = async (area) => {
     try {
+      setError(null);
+
       const apiKey = '4f687960aac453f859c90cdb61eb395b';
-      const city = area.split('(')[0].trim(); 
+      const city = area.split('(')[0].trim();
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
       );
-  
+
+      if (!response.ok) {
+        throw new Error(`Weather data request failed no data available ${response.status}`);
+      }
+
       const data = await response.json();
       setWeatherData(data);
-  
+
       const cityTime = new Date(data.dt * 1000).toLocaleTimeString();
       setSelectedAreas((prevSelectedAreas) => {
         const newSelectedAreas = new SelectedAreas();
         newSelectedAreas.selectedAreas = [...prevSelectedAreas.getAreas()];
         const index = newSelectedAreas.selectedAreas.indexOf(area);
         if (index !== -1) {
-          newSelectedAreas.selectedAreas[index] = city + ' (' + cityTime + ')';
+          newSelectedAreas.selectedAreas[index] = `${city} (${cityTime})`;
         }
         return newSelectedAreas;
       });
     } catch (error) {
+      setError(error.message);
       console.error('Error fetching weather data:', error);
     }
   };
-  
+
   return (
     <div className="weather-app">
       <h1>Today's Weather</h1>
@@ -85,26 +101,36 @@ function WeatherApp() {
         />
         <button onClick={handleSearch}>Search</button>
       </div>
-      {weatherData && (
-        <div className="weather-data">
-          <h2>{weatherData.name}</h2>
-          <p>Weather: {weatherData.weather[0].description}</p>
-          <p>Temperature: {weatherData.main.temp}°F</p>
-          <p>Time: {new Date().toLocaleTimeString()}</p>
-        </div>
-      )}
+      {error ? (
+  <p className="error-message">{error}</p>
+) : (
+  weatherData && (
+    <div className="weather-data">
+      <h2>{weatherData.name}</h2>
+      <p>Weather: {weatherData.weather[0].description}</p>
+      <p>Temperature: {weatherData.main.temp}°F</p>
+      <p>Time: {new Date().toLocaleTimeString()}</p>
+    </div>
+  )
+)}
+
       <div className="selected-areas">
         <h3>Saved Locations</h3>
         {selectedAreas.getAreas().map((area) => (
           <div key={area} className="area-item">
             <span>{area}</span>
             <button onClick={() => handleRefresh(area)}>Refresh</button>
-            <button onClick={() => setSelectedAreas((prevSelectedAreas) => {
-              const newSelectedAreas = new SelectedAreas();
-              newSelectedAreas.selectedAreas = [...prevSelectedAreas.getAreas()];
-              newSelectedAreas.removeArea(area);
-              return newSelectedAreas;
-            })}>Remove</button>
+            <button
+              onClick={() => {
+                
+                const newSelectedAreas = new SelectedAreas();
+                newSelectedAreas.selectedAreas = [...selectedAreas.getAreas()];
+                newSelectedAreas.removeArea(area);
+                setSelectedAreas(newSelectedAreas);
+              }}
+            >
+              Remove
+            </button>
           </div>
         ))}
       </div>
@@ -113,3 +139,4 @@ function WeatherApp() {
 }
 
 export default WeatherApp;
+
